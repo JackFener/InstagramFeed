c388ff9af8fdd4efeaa7da510098bae257f6a9ac
JackFener <giacomofava93@gmail.com>
2016-12-23 17:26:41 +0100

    includeMe.php
    include me where you want to show your ig feed

---
+++ includeMe.php
@@ -0,0 +1,9 @@
+<?php
+//include me where you want to show your ig feed
+                    $RIGHE=$_GET["r"];
+                    $COLONNE=$_GET["c"];;
+                    $HASHTAG=$_GET["hash"];
+                    $LARG=$_GET["w"];
+                    $ALT=$_GET["h"];
+                    include('http://jackontherocks.tk/InstaFeed/instaFeed.php');
+?>
